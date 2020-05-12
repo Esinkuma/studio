c44fa08038ec1eab5c377a1a448e025917a468bb
Esinkuma Nyananyo <esinkuma@enovatengr.com>
2020-05-13 00:16:36 +0100

    Add dialogflow middleware and action

--- routes/botman.php
+++ routes/botman.php
@@ -1,9 +1,26 @@
 <?php
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Middleware\ApiAi;
 use App\Http\Controllers\BotManController;
 
+$dialogFlowToken = env('DIALOG_FLOW_TOKEN');
+
+$dialogFlow = ApiAi::create($dialogFlowToken)->listenForAction();
+
+/** @var BotMan $botman */
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
-});
+$botman->middleware->received($dialogFlow);
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->hears('input.welcome', function (BotMan $bot){
+
+$extras = $bot->getMessage()->getExtras();
+$apiReply = $extras['apiReply'];
+$apiAction = $extras['apiAction'];
+$apiIntent = $extras['apiIntent'];
+
+$bot->reply($apiReply);
+})->middleware($dialogFlow);
